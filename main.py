22ff005f0f21b9b6fdc17e43215a12dd72e90fee
shui-dun <2604697844@qq.com>
2021-12-06 14:17:32 +0800

    改为面向对象

--- main.py
+++ main.py
@@ -1,54 +1,52 @@
 from pydub import AudioSegment
 from gtts import gTTS
 from io import BytesIO
-import os
-import time as t
-
-audio = AudioSegment.silent(duration=1000)
 
 
-def addSegment(text, time=0):
-    global audio
-    tts = gTTS(text=text, lang='zh-tw')
-    mp3_fp = BytesIO()
-    tts.write_to_fp(mp3_fp)
-    mp3_fp.seek(0)
-    add = AudioSegment.from_mp3(mp3_fp)
-    silent = AudioSegment.silent(duration=int(time * 1000 * 60))
-    audio = audio + add + silent
+class Meditation:
+    audio = AudioSegment.silent(duration=1000)
 
+    def addSegment(self, text, time=0.0):
+        tts = gTTS(text=text, lang='zh-tw')
+        mp3_fp = BytesIO()
+        tts.write_to_fp(mp3_fp)
+        mp3_fp.seek(0)
+        add = AudioSegment.from_mp3(mp3_fp)
+        silent = AudioSegment.silent(duration=int(time * 1000 * 60))
+        self.audio = self.audio + add + silent
 
-def save(audioName):
-    audio.export(audioName, format="mp3")
+    def save(self, audioName):
+        self.audio.export(audioName, format="mp3")
 
 
 def meditation1():
-    addSegment('找到舒服的姿势', 1 / 60)
-    addSegment('开始深呼吸', 1)
-    addSegment('开始环境冥想', 2)
-    addSegment('开始视觉冥想', 4)
-    addSegment('开始感恩冥想', 4)
-    addSegment('开始慈爱冥想', 2)
-    addSegment('开始放松意识', 4)
-    addSegment('开始环境冥想', 2)
-    addSegment('完成冥想')
-    save("meditation1.mp3")
+    m = Meditation()
+    m.addSegment('找到舒服的姿势', 5 / 60)
+    m.addSegment('开始深呼吸', 1)
+    m.addSegment('开始环境冥想', 2)
+    m.addSegment('开始视觉冥想', 4)
+    m.addSegment('开始感恩冥想', 4)
+    m.addSegment('开始慈爱冥想', 2)
+    m.addSegment('开始放松意识', 4)
+    m.addSegment('开始环境冥想', 2)
+    m.addSegment('完成冥想')
+    m.save("meditation1.mp3")
 
 
 def meditation2():
-    addSegment('找到舒服的姿势', 1 / 60)
-    addSegment('开始深呼吸', 1)
-    addSegment('开始环境冥想', 2)
-    addSegment('开始身体扫描', 3)
-    addSegment('开始慈悲冥想', 3)
-    addSegment('开始专注冥想', 4)
-    addSegment('开始放松意识', 4)
-    addSegment('开始环境冥想', 2)
-    addSegment('完成冥想')
-    save("meditation2.mp3")
+    m = Meditation()
+    m.addSegment('找到舒服的姿势', 5 / 60)
+    m.addSegment('开始深呼吸', 1)
+    m.addSegment('开始环境冥想', 2)
+    m.addSegment('开始身体扫描', 3)
+    m.addSegment('开始慈悲冥想', 3)
+    m.addSegment('开始专注冥想', 4)
+    m.addSegment('开始放松意识', 4)
+    m.addSegment('开始环境冥想', 2)
+    m.addSegment('完成冥想')
+    m.save("meditation2.mp3")
 
 
 if __name__ == '__main__':
-    start = t.time()
+    meditation1()
     meditation2()
-    print(t.time() - start)
