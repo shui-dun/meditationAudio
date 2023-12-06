dc6376775c6164d5b14636645fa766d203a58e3a
shui-dun <2604697844@qq.com>
2023-12-07 00:16:04 +0800

    Refactor parseText function to handle titles correctly

--- parseText.py
+++ parseText.py
@@ -6,8 +6,8 @@ def parseText():
     with open('content.md', 'r', encoding='utf-8') as f:
         for line in f:
             line = line.strip()
-            # 如果line以*或者-或者数字开头，则认为是列表项
-            isList = re.match(r'^[\-*\d.]+', line)
+            # 如果line以#或者>开头，那么就是标题
+            isTitle = re.match(r'^[>#]', line)
             # 将[xxx](yyy)转化为xxx
             line = re.sub(r'\[(.*?)\]\(.*?\)', r'\1', line)
             # 删除前导的#号、>号、空格、-号、*号、数字、点号
@@ -15,7 +15,7 @@ def parseText():
             # 删除所有的*
             line = re.sub(r'\*', '', line)
             if len(line) > 0:
-                ans.append((line, 6 if isList else 0, False))
+                ans.append((line, 0 if isTitle else 6, False))
     print(ans)
     return ans
 
